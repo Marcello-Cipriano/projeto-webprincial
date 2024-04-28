1f02430cb2e938388f41ae8023f3b44c1ab0e0bc
Marcello Cipriano <marcello0408cipriano@hotmail.com>
2024-04-28 20:39:42 -0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,74 +1,107 @@
 import React, { useState } from 'react';
-import './cadastro.css'; // Importe sua folha de estilo
+import { useNavigate } from 'react-router-dom'; 
+import './cadastro.css';
+import nossoRecifeLogo from '../images/Nosso Recife.png';
 
 const Home = () => {
-  const [modoEscuro, setModoEscuro] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+  const [error, setError] = useState('');
+  const navigate = useNavigate(); 
 
-  const alternarModoEscuro = () => {
-    setModoEscuro(!modoEscuro);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Verifica se o email é válido
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(formData.email)) {
+      setError("Por favor, insira um email válido.");
+      return;
+    }
+
+    
+    navigate("/conhecer"); 
   };
 
   return (
-    <main id="container" className={modoEscuro ? 'dark' : ''}>
-      <form id="login_form">
+    <main id="container">
+      <form id="login_form" onSubmit={handleSubmit}>
         <div id="form_header">
-          <img src="imagens/Nosso Recife.png" alt="Logo" style={{ width: '150px', position: 'absolute', top: '10px', left: '10px' }} />
+          <img
+            src={nossoRecifeLogo}
+            className="navbar-logo"
+            alt="Logo do Nosso Recife"
+            style={{ width: 150, position: 'absolute', top: 10, left: 10 }}
+          />
           <h1>Cadastro</h1>
-          <i id="mode_icon" className={modoEscuro ? 'fa-solid fa-sun' : 'fa-solid fa-moon'} onClick={alternarModoEscuro}></i>
-        </div>
-
-        <div id="social_media">
-          <a href="#">
-            <img src="imagens/facebook logo.png" alt="" />
-          </a>
-          <a href="#">
-            <img src="imagens/google logo.png" alt="Logotipo do Google" />
-          </a>
         </div>
 
         <div id="inputs">
+          {error && <p className="error">{error}</p>}
           <div className="input-box">
-            <label for="name">
-              Nome
-              <div className="input-field">
-                <i className="fa-solid fa-user"></i>
-                <input type="text" id="name" name="name" />
-              </div>
-            </label>
+            <label htmlFor="name">Nome</label>
+            <div className="input-field">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                autoComplete="name"
+                placeholder="Digite seu nome"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </div>
           </div>
 
           <div className="input-box">
-            <label for="email">
-              E-mail
-              <div className="input-field">
-                <i className="fa-solid fa-envelope"></i>
-                <input type="email" id="email" name="email" />
-              </div>
-            </label>
+            <label htmlFor="email">E-mail</label>
+            <div className="input-field">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                autoComplete="email"
+                placeholder="Digite seu e-mail"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
           </div>
 
           <div className="input-box">
-            <label for="password">
-              Senha
-              <div className="input-field">
-                <i className="fa-solid fa-key"></i>
-                <input type="password" id="password" name="password" />
-              </div>
-            </label>
-
+            <label htmlFor="password">Senha</label>
+            <div className="input-field">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                autoComplete="off"
+                placeholder="Digite sua senha"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
             <div id="forgot_password">
               <a href="#">Esqueci a senha</a>
             </div>
           </div>
         </div>
 
-        <button type="submit" id="login_button">
-          Login
-        </button>
+        <button type="submit" id="login_button">Login</button>
       </form>
     </main>
   );
 };
 
 export default Home;
-
